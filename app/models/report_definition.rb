45149a79847d077bc7dd4cc59b877d59007eb9bf
dustin <dstywho@gmail.com>
2013-05-05 16:54:34 -0400

    adding emails to report definition

--- app/models/report_definition.rb
+++ app/models/report_definition.rb
@@ -1,5 +1,5 @@
 class ReportDefinition < ActiveRecord::Base
-  attr_accessible :name, :reports
+  attr_accessible :name, :reports, :emails
   has_many :reports
   has_many :report_definition_test_plans 
   has_many :test_plans, :through => :report_definition_test_plans 
--- db/migrate/20130505125800_create_report_definitions.rb
+++ db/migrate/20130505125800_create_report_definitions.rb
@@ -2,6 +2,7 @@ class CreateReportDefinitions < ActiveRecord::Migration
   def change
     create_table :report_definitions do |t|
       t.string :name
+      t.string :emails
 
       t.timestamps
     end
