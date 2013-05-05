ff28dfee771d7982f7bba0afa138e0141f007ad6
dustin <dstywho@gmail.com>
2013-05-05 10:15:41 -0400

    wip

--- app/models/report_definition.rb
+++ app/models/report_definition.rb
@@ -1,5 +1,6 @@
 class ReportDefinition < ActiveRecord::Base
   attr_accessible :name
   has_many :reports
-  has_many :test_plans, :through => :report_definition_test_plan 
+  has_many :report_definition_test_plans 
+  has_many :test_plans, :through => :report_definition_test_plans 
 end
